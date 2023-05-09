8453eff0cf259c6ff64b8cb20afbd45041e66ad7
Khess S Dotto <khessdotto@gmail.com>
2023-05-08 22:08:42 -0300

    Reduced text on aboutMeExperience after feedback.

--- components/aboutMeExperience/index.tsx
+++ components/aboutMeExperience/index.tsx
@@ -3,7 +3,7 @@ import { Fade } from "react-awesome-reveal";
 export default function AboutMeExperience() {
   return (
     <article className="flex flex-col items-center justify-center px-6 gap-4 pb-6">
-      <Fade duration={1000}>
+      <Fade duration={1000} delay={300}>
         <h3 className="underline font-semibold text-xl text-gray-700 flex flex-row items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -15,40 +15,33 @@ export default function AboutMeExperience() {
           </svg>
           Minha história/experiência
         </h3>
-        <div className="flex flex-col gap-1 text-sm leading-5 indent-4 text-justify">
+        <div className="flex flex-col gap-4 text-sm leading-relaxed indent-4 text-justify">
           <p>
             Comecei minha carreira no mercado financeiro com uma oportunidade
             que recebi pela faculdade (FGV). Apesar de possuir um perfil
             naturalmente mais analítico, neste mercado, desenvolvi bastante o
-            meu lado comercial e comunicativo. Infelizmente, não era onde eu
-            queria construir a minha carreira... :(
+            meu lado comunicativo. Infelizmente, não era onde eu gostaria de
+            construir a minha carreira... :(
           </p>
           <p>
-            Em meus estudos paralelos, fiz um curso de VBA para automatizar
-            processos manuais / mecânicos que tínhamos no banco. Isso me
-            interessou muito e vi grande importância!
+            Em meus estudos, fiz um curso de VBA para automatizar processos
+            manuais que tínhamos no banco. Isso me interessou muito e vi grande
+            importância! Pelos próximos 2 anos, mantive contato próximo com a
+            programação (VBA e Python). Era a parte que eu mais gostava no meu
+            trabalho, bem como desenvolver planilhas de cálculos intuitivas para
+            uso da equipe.
           </p>
           <p>
-            Pelos próximos 2 anos, mantive contato próximo com a programação
-            (VBA e Python) e a parte que eu mais gostava do meu trabalho era de
-            lidar diretamente com códigos para automatizar processos, bem como
-            desenvolver planilhas de cálculos intuitivas e esteticamente
-            agradáveis para uso da equipe.
+            A partir desse momento, cursei matérias eletivas de desenvolvimento
+            Web na faculdade e, por fim, concluí o curso de Web Development full
+            stack na Ironhack no início de 2023.
           </p>
           <p>
-            A partir desse momento, aprendi e resolvi estudar por conta própria
-            desenvolvimento Web Full Stack, área que me interessou muito! cursei
-            matérias eletivas de desenvolvimento Web na faculdade e, por fim,
-            concluí o curso completo de de Web Development full stack na
-            Ironhack no início de 2023.
-          </p>
-          <p>
-            Aprendi bastante e estou desenvolvendo uma série de projetos
-            pessoais enquanto não tenho a minha primeira oportunidade de
-            emprego! Continuo estudando todos os dias, tenho cerca de 3 anos de
+            Continuo estudando todos os dias, tenho cerca de 3 anos de
             experiência com programação em geral e 1 ano especificamente com
-            desenvolvimento web. Quer saber mais? Me chama e vamos conversar!!!
-            Meus contatos estão ao final (rodapé).
+            desenvolvimento web! À procura do primeiro emprego... Quer saber
+            mais? Me chame e vamos conversar!!! Meus contatos estão ao final
+            (rodapé).
           </p>
         </div>
       </Fade>
