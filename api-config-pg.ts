0b544606810ea27861998cd901d296eed8b74c6a
Khess S Dotto <khessdotto@gmail.com>
2023-05-16 17:26:26 -0300

    Fixed API to connect with Vercel Postgres.

--- api-config-pg.ts
+++
@@ -1,5 +0,0 @@
-import { Pool } from "pg";
-
-export const pool: Pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-});
--- pages/api/comment/[id].ts
+++ pages/api/comment/[id].ts
@@ -1,31 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { QueryResult } from "pg";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { pool } from "../../../api-config-pg";
-
-interface DataRow {
-  id: number;
-  comment: string;
-  commenter: string;
-}
+import { db } from "@vercel/postgres";
 
 export default async function GetOne(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const client = await db.connect();
   try {
     if (req.method !== "GET") {
       res.status(405).json("Method not allowed");
     }
-
     const { id } = req.query;
-
-    const comment: QueryResult = await pool.query(
-      `SELECT * FROM comments WHERE id = ${id}`
-    );
-
+    const comment = await client.sql`SELECT * FROM comments WHERE id = ${String(
+      id
+    )};`;
     const commentRow = comment.rows;
-
     res.status(200).json(commentRow);
   } catch (err) {
     console.log(err);
